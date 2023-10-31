6ae1625a04df32c3abbeaa7f0ac6be21b8727683
Raketa <mr.raketa458x@gmail.com>
2023-10-31 13:11:44 +0200

    worked task 04

--- 04_telegram_bot_ weather_forecast/WeatherController.js
+++ 04_telegram_bot_ weather_forecast/WeatherController.js
@@ -2,15 +2,14 @@ import axios from 'axios';
 
 class WeatherController {
 
-    static appid = "64eb7f71512192e5ac6303e3c2537ecf"
-    static city = "Odesa"
-    static units = 'metric'
+    appid = "64eb7f71512192e5ac6303e3c2537ecf"
+    city = "Odesa"
+    units = 'metric'
 
     async getWeather() {
         try {
-            const response = await axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${this.city}&APPID=${this.appid}&units=${this.units}`);
-
-            let currentWeather = {
+            const response = await axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${this.city}&APPID=${this.appid}&units=${this.units}`)
+            const currentWeather = {
                 main: response.data.weather[0].main,
                 description: response.data.weather[0].description,
                 icon: response.data.weather[0].icon,
@@ -21,35 +20,28 @@ class WeatherController {
                 humidity: response.data.main.humidity,
                 wind: response.data.wind.speed
             }
-
             return currentWeather;
         } catch (error) {
             console.error(error);
         }
-
     }
-
-    async getForecast() {
+    async getForecast(hour) {
         try {
-            let data = []
             const response = await axios.get(`http://api.openweathermap.org/data/2.5/forecast?q=${this.city}&appid=${this.appid}&units=${this.units}`)
-            for (let i = 0; i <= 5; ++i) {
-                data.push({
-                    temp: response.data.list[i].main.temp,
-                    feels_like: response.data.list[i].main.feels_like,
-                    temp_min: response.data.list[i].main.temp_min,
-                    temp_max: response.data.list[i].main.temp_max,
-                    humidity: response.data.list[i].main.humidity,
-                    date: response.data.list[i].dt_txt,
-                    weather: response.data.list[i].weather[0].main
-                })
+            const data = {
+                temp: response.data.list[hour].main.temp,
+                feels_like: response.data.list[hour].main.feels_like,
+                temp_min: response.data.list[hour].main.temp_min,
+                temp_max: response.data.list[hour].main.temp_max,
+                humidity: response.data.list[hour].main.humidity,
+                date: response.data.list[hour].dt_txt,
+                weather: response.data.list[hour].weather[0].main,
+                wind: response.data.list[hour].wind.speed
             }
-
             return data;
         } catch (error) {
             console.error(error);
         }
     }
 }
-
 export default WeatherController;
--- 04_telegram_bot_ weather_forecast/app.js
+++ 04_telegram_bot_ weather_forecast/app.js
@@ -24,35 +24,53 @@ bot.setMyCommands(
 )
 
 bot.on('message', async (msg) => {
+    try {
+        let chatId = msg.chat.id
+        let text = msg.text
 
+        if (text === "/start") {
+            //bot.sendSticker(chatId, 'https://tlgrm.ru/_/stickers/4a4/5eb/4a45ebcb-39f0-391d-bb2b-60fcf0e8100a/1.webp')
+            return bot.sendMessage(chatId, "Hello, Master")
+        }
 
-    let text = msg.text
-
-    if (text === "/start") {
-        bot.sendSticker(chatId, 'https://tlgrm.ru/_/stickers/4a4/5eb/4a45ebcb-39f0-391d-bb2b-60fcf0e8100a/1.webp')
-        return bot.sendMessage(chatId, "Hello, Master")
+        if (text === "/weather") {
+            return bot.sendMessage(chatId, "Make you choise, Master", weatherOptions)
+        }
+        return bot.sendMessage(chatId, "I dont understand you, Master")
+    } catch (error) {
+        console.log(error);
     }
-
-    if (text === "/weather") {
-        return bot.sendMessage(chatId, "Make you choise, Master", weatherOptions)
-    }
-    return bot.sendMessage(chatId, "I dont understand you, Master")
 })
-
 bot.on('callback_query', async (msg) => {
-
-    let chatId = msg.message.chat.id
-    let data = msg.data
-
-    if (data === '1') {
-        return bot.sendMessage(chatId, `You choose ${data}`)
+    try {
+        const chatId = msg.message.chat.id
+        const data = msg.data
+        await bot.sendMessage(chatId, `I am working on the request...`)
+        if (data === '1') {
+            const currentWeather = await wc.getWeather().catch(err => console.error(err))
+            return bot.sendMessage(chatId, `Current weather: ${currentWeather.main} \n 
+            Temp: ${currentWeather.temp} C°\n 
+            Feels like: ${currentWeather.feels_like} C°\n
+            Humidity: ${currentWeather.humidity}% \n 
+            Wind: ${currentWeather.wind} m/s \n`)
+        }
+        if (data === '2') {
+            const currentForecast = await wc.getForecast(1).catch(err => console.error(err))
+            await bot.sendMessage(chatId, `Forecast in 3 hours: ${currentForecast.weather} \n 
+            Temp: ${currentForecast.temp} C°\n 
+            Feels like: ${currentForecast.feels_like} C°\n
+            Humidity: ${currentForecast.humidity}% \n 
+            Wind: ${currentForecast.wind} m/s \n`)
+        }
+        if (data === '3') {
+            const currentForecast = await wc.getForecast(2).catch(err => console.error(err))
+            await bot.sendMessage(chatId, `Forecast in 6 hours: ${currentForecast.weather} \n 
+            Temp: ${currentForecast.temp} C°\n 
+            Feels like: ${currentForecast.feels_like} C°\n
+            Humidity: ${currentForecast.humidity}% \n 
+            Wind: ${currentForecast.wind} m/s \n`)
+        }
+    } catch (error) {
+        console.log(error);
     }
-    if (data === '2') {
-        return bot.sendMessage(chatId, `You choose ${data}`)
-    }
-    if (data === '3') {
-        return bot.sendMessage(chatId, `You choose ${data}`)
-    }
-
-
 })
